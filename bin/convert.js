3d45e586507aa30154f07232fb2b35d5e97f8fe2
Aleksandr Vasilenko <phenomen@proton.me>
2023-04-13 14:39:58 -0500

    Rome Tools formatting/linting

--- bin/convert.js
+++ bin/convert.js
@@ -3,132 +3,162 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { spinner } from "@clack/prompts";
 
-const formatsImage = [".avif", ".jpeg", ".jpg", ".png", ".webp", ".tiff"];
-const formatsVideo = [".avi", ".m4v", ".mov", ".mp4", ".mpeg", ".webm"];
-const formatsAudio = [...formatsVideo, ".m4a", ".mp3", ".wav", ".flac", ".ogg"];
-
 export async function convert(userFolder, userFormat, userQuality) {
-  // SETTINGS
+	const formatsImage = [
+		".avif",
+		".gif",
+		".jpeg",
+		".jpg",
+		".png",
+		".tiff",
+		".webp",
+	];
+	const formatsVideo = [
+		".avi",
+		".m4v",
+		".mkv",
+		".mov",
+		".mp4",
+		".mpeg",
+		".webm",
+		".wmv",
+	];
+	const formatsAudio = [
+		...formatsVideo,
+		".aac",
+		".flac",
+		".m4a",
+		".mp3",
+		".ogg",
+		".wav",
+	];
 
-  let action;
-  let compatibleFormats = [];
-  let extra = [];
-  let quality = [];
+	let action;
+	let compatibleFormats = [];
+	let extra = [];
+	let quality = [];
 
-  if (["webp", "png", "avif", "jpg", "jpeg"].includes(userFormat)) {
-    action = "image";
-    compatibleFormats = formatsImage;
+	// SETTINGS
 
-    if (userQuality === "high") {
-      quality = ["-q", "90"];
-    } else if (userQuality === "mid") {
-      quality = ["-q", "75"];
-    } else if (userQuality === "low") {
-      quality = ["-q", "60"];
-    }
-  } else if (["webm", "mp4"].includes(userFormat)) {
-    action = "video";
-    compatibleFormats = formatsVideo;
+	if (["webp", "png", "avif", "jpg", "jpeg"].includes(userFormat)) {
+		action = "image";
+		compatibleFormats = formatsImage;
 
-    if (userQuality === "high") {
-      quality = ["-b:v", "2M", "-b:a", "128k"];
-    } else if (userQuality === "mid") {
-      quality = ["-b:v", "1M", "-b:a", "128k"];
-    } else if (userQuality === "low") {
-      quality = ["-b:v", "512k", "-b:a", "64k"];
-    }
-  } else if (["mp3", "ogg"].includes(userFormat)) {
-    action = "audio";
-    compatibleFormats = formatsAudio;
+		if (userQuality === "high") {
+			quality = ["-q", "90"];
+		} else if (userQuality === "mid") {
+			quality = ["-q", "75"];
+		} else if (userQuality === "low") {
+			quality = ["-q", "60"];
+		}
+	} else if (["webm", "mp4"].includes(userFormat)) {
+		action = "video";
+		compatibleFormats = formatsVideo;
 
-    if (userQuality === "high") {
-      quality = ["-b:a", "192k"];
-    } else if (userQuality === "mid") {
-      quality = ["-b:a", "128k"];
-    } else if (userQuality === "low") {
-      quality = ["-b:a", "64k"];
-    }
-  } else {
-    console.error("Unknown output format");
-    process.exit(0);
-  }
+		if (userQuality === "high") {
+			quality = ["-b:v", "2M", "-b:a", "128k"];
+		} else if (userQuality === "mid") {
+			quality = ["-b:v", "1M", "-b:a", "128k"];
+		} else if (userQuality === "low") {
+			quality = ["-b:v", "512k", "-b:a", "64k"];
+		}
+	} else if (["mp3", "ogg"].includes(userFormat)) {
+		action = "audio";
+		compatibleFormats = formatsAudio;
 
-  if (userFormat === "webm") {
-    extra = ["-c:v", "libvpx", "-c:a", "libvorbis", "-row-mt", "1", "-f", "webm"];
-  } else if (userFormat === "mp4") {
-    extra = ["-c:a", "libvorbis"];
-  } else if (userFormat === "ogg") {
-    extra = ["-vn", "-c:a", "libvorbis"];
-  } else if (userFormat === "mp3") {
-    extra = ["-vn"];
-  }
+		if (userQuality === "high") {
+			quality = ["-b:a", "192k"];
+		} else if (userQuality === "mid") {
+			quality = ["-b:a", "128k"];
+		} else if (userQuality === "low") {
+			quality = ["-b:a", "64k"];
+		}
+	} else {
+		console.error("Unknown output format");
+		process.exit(0);
+	}
 
-  // FILES
+	if (userFormat === "webm") {
+		extra = [
+			"-c:v",
+			"libvpx",
+			"-c:a",
+			"libvorbis",
+			"-row-mt",
+			"1",
+			"-f",
+			"webm",
+		];
+	} else if (userFormat === "mp4") {
+		extra = ["-c:a", "libvorbis"];
+	} else if (userFormat === "ogg") {
+		extra = ["-vn", "-c:a", "libvorbis"];
+	} else if (userFormat === "mp3") {
+		extra = ["-vn"];
+	}
 
-  const inputDir = path.join(userFolder);
-  const outputDir = path.join(userFolder, "output");
+	// FILES
 
-  const files = await fs.readdir(inputDir);
+	const inputDir = path.normalize(userFolder);
+	const outputDir = path.join(userFolder, "output");
 
-  const compatibleFiles = files.filter((file) =>
-    compatibleFormats.includes(path.extname(file).toLowerCase())
-  );
+	const files = await fs.readdir(inputDir);
 
-  if (compatibleFiles.length === 0) {
-    console.error("No compatible files found");
-    process.exit(0);
-  }
+	const compatibleFiles = files.filter((file) =>
+		compatibleFormats.includes(path.extname(file).toLowerCase()),
+	);
 
-  async function createOutputDir() {
-    try {
-      await fs.mkdir(outputDir);
-    } catch (error) {
-      if (error.code === "EEXIST") {
-        console.log("Output directory already exists");
-      } else {
-        console.error(error);
-        process.exit(1);
-      }
-    }
-  }
+	if (compatibleFiles.length === 0) {
+		console.error("No compatible files found");
+		process.exit(0);
+	}
 
-  await createOutputDir();
+	async function createOutputDir() {
+		try {
+			await fs.mkdir(outputDir);
+		} catch (error) {
+			if (error.code === "EEXIST") {
+				console.log("Output directory already exists");
+			} else {
+				console.error(error);
+				process.exit(1);
+			}
+		}
+	}
 
-  // FFMPEG
+	await createOutputDir();
 
-  const ffmpeg = createFFmpeg({ log: true });
+	// FFMPEG
 
-  await ffmpeg.load();
+	const ffmpeg = createFFmpeg({ log: false });
 
-  const s = spinner();
+	await ffmpeg.load();
 
-  s.start(`Converting ${compatibleFiles.length} file(s) into ${userFormat}`);
+	const s = spinner();
 
-  for (let i = 0; i < compatibleFiles.length; i++) {
-    const inputFile = path.join(inputDir, compatibleFiles[i]);
-    const outputFile = path.join(
-      outputDir,
-      compatibleFiles[i].replace(/\.[^/.]+$/, "") + "." + userFormat
-    );
+	s.start(`Converting ${compatibleFiles.length} file(s) into ${userFormat}`);
 
-    ffmpeg.FS("writeFile", compatibleFiles[i], await fetchFile(inputFile));
+	for (let i = 0; i < compatibleFiles.length; i++) {
+		const inputFile = path.join(inputDir, compatibleFiles[i]);
+		const outputFile = path.join(
+			outputDir,
+			`${compatibleFiles[i].replace(/\.[^/.]+$/, "")}.${userFormat}`,
+		);
 
-    if (action === "image") {
-      await ffmpeg.run("-i", compatibleFiles[i], ...quality, outputFile);
-    }
+		ffmpeg.FS("writeFile", compatibleFiles[i], await fetchFile(inputFile));
 
-    if (action === "video") {
-      await ffmpeg.run("-i", compatibleFiles[i], ...quality, ...extra, outputFile);
-    }
+		await ffmpeg.run(
+			"-i",
+			compatibleFiles[i],
+			...quality,
+			...extra,
+			outputFile,
+		);
 
-    if (action === "audio") {
-      await ffmpeg.run("-i", compatibleFiles[i], ...quality, ...extra, outputFile);
-    }
+		await fs.writeFile(outputFile, ffmpeg.FS("readFile", outputFile));
 
-    await fs.writeFile(outputFile, ffmpeg.FS("readFile", outputFile));
+		ffmpeg.FS("unlink", outputFile);
+	}
 
-    ffmpeg.FS("unlink", outputFile);
-  }
-
-  s.stop(`Converted ${compatibleFiles.length} file(s)`);
+	s.stop(`Converted ${compatibleFiles.length} file(s)`);
 }
--- bin/index.js
+++ bin/index.js
@@ -3,58 +3,58 @@ import { intro, select, isCancel, cancel, text, outro } from "@clack/prompts";
 import { convert } from "./convert.js";
 
 async function main() {
-  console.log();
-  intro(" [ VTTC ] ");
+	console.log();
+	intro(" [ VTTC ] ");
 
-  const folder = await text({
-    message: "Select an input folder",
-    placeholder: "./",
-    defaultValue: "./",
-    initialValue: "./",
-  });
+	const folder = await text({
+		message: "Select an input folder",
+		placeholder: "./",
+		defaultValue: "./",
+		initialValue: "./",
+	});
 
-  if (isCancel(folder)) {
-    cancel("Operation cancelled");
-    return process.exit(0);
-  }
+	if (isCancel(folder)) {
+		cancel("Operation cancelled");
+		return process.exit(0);
+	}
 
-  const format = await select({
-    message: "Select an output format",
-    options: [
-      { value: "webp", label: "WEBP", hint: "Recommended for images" },
-      { value: "png", label: "PNG" },
-      { value: "jpeg", label: "JPEG" },
-      { value: "webm", label: "WEBM", hint: "Recommended for video" },
-      { value: "mp4", label: "MP4" },
-      { value: "ogg", label: "OGG", hint: "Recommended for audio" },
-      { value: "mp3", label: "MP3" },
-    ],
-  });
+	const format = await select({
+		message: "Select an output format",
+		options: [
+			{ value: "webp", label: "WEBP", hint: "Recommended for images" },
+			{ value: "png", label: "PNG" },
+			{ value: "jpeg", label: "JPEG" },
+			{ value: "webm", label: "WEBM", hint: "Recommended for video" },
+			{ value: "mp4", label: "MP4" },
+			{ value: "ogg", label: "OGG", hint: "Recommended for audio" },
+			{ value: "mp3", label: "MP3" },
+		],
+	});
 
-  if (isCancel(format)) {
-    cancel("Operation cancelled");
-    return process.exit(0);
-  }
+	if (isCancel(format)) {
+		cancel("Operation cancelled");
+		return process.exit(0);
+	}
 
-  const quality = await select({
-    message: "Select a quality",
-    options: [
-      { value: "high", label: "HIGH" },
-      { value: "mid", label: "MEDIUM", hint: "Recommended" },
-      { value: "low", label: "LOW" },
-    ],
-    initialValue: "mid",
-  });
+	const quality = await select({
+		message: "Select a quality",
+		options: [
+			{ value: "high", label: "HIGH" },
+			{ value: "mid", label: "MEDIUM", hint: "Recommended" },
+			{ value: "low", label: "LOW" },
+		],
+		initialValue: "mid",
+	});
 
-  if (isCancel(quality)) {
-    cancel("Operation cancelled");
-    return process.exit(0);
-  }
+	if (isCancel(quality)) {
+		cancel("Operation cancelled");
+		return process.exit(0);
+	}
 
-  await convert(folder, format, quality);
+	await convert(folder, format, quality);
 
-  outro("Your converted files are in the 'output' folder.");
+	outro("Your converted files are in the 'output' folder.");
 
-  process.exit(0);
+	process.exit(0);
 }
 main().catch(console.error);
