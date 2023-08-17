fd15a77a5dfb00328f246d937e957b13152af996
Aleksandr Vasilenko <phenomen@pm.me>
2023-08-17 15:06:49 -0500

    FFMpeg 6, libopus

--- bin/convert.js
+++ bin/convert.js
@@ -1,175 +1,213 @@
-import sharp from 'sharp';
-import * as fsp from 'node:fs/promises';
-import fs from 'node:fs';
-import path from 'node:path';
-import { spinner } from '@clack/prompts';
-
-import ffmpegPath from 'ffmpeg-static';
-import ffmpegFluent from 'fluent-ffmpeg';
+import sharp from "sharp";
+import * as fsp from "node:fs/promises";
+import fs from "node:fs";
+import path from "node:path";
+import { spinner } from "@clack/prompts";
+import ffmpegPath from "ffmpeg-static";
+import ffmpegFluent from "fluent-ffmpeg";
 
 export async function convert(userFolder, userFormat, userQuality) {
-	const formatsImage = ['.avif', '.gif', '.jpeg', '.jpg', '.png', '.tiff', '.webp', '.jfif'];
-	const formatsVideo = ['.avi', '.m4v', '.mkv', '.mov', '.mp4', '.mpeg', '.webm', '.wmv'];
-	const formatsAudio = [...formatsVideo, '.aac', '.flac', '.m4a', '.mp3', '.ogg', '.wav'];
+  const formatsImage = [
+    ".avif",
+    ".gif",
+    ".jpeg",
+    ".jpg",
+    ".png",
+    ".tiff",
+    ".webp",
+    ".jfif",
+  ];
+  const formatsVideo = [
+    ".avi",
+    ".m4v",
+    ".mkv",
+    ".mov",
+    ".mp4",
+    ".mpeg",
+    ".webm",
+    ".wmv",
+  ];
+  const formatsAudio = [
+    ...formatsVideo,
+    ".aac",
+    ".flac",
+    ".m4a",
+    ".mp3",
+    ".ogg",
+    ".wav",
+  ];
 
-	let action;
-	let compatibleFormats = [];
-	let extra = [];
-	let quality;
+  const ffmpeg = ffmpegFluent;
+  ffmpeg.setFfmpegPath(ffmpegPath);
 
-	// SETTINGS
+  let action;
+  let compatibleFormats = [];
+  let extra = [];
+  let quality;
 
-	if (['webp', 'png', 'avif', 'jpg', 'jpeg', 'jfif'].includes(userFormat)) {
-		action = 'image';
-		compatibleFormats = formatsImage;
+  const s = spinner();
 
-		if (userQuality === 'high') {
-			quality = 90;
-		} else if (userQuality === 'mid') {
-			quality = 80;
-		} else if (userQuality === 'low') {
-			quality = 60;
-		}
-	} else if (['webm', 'mp4'].includes(userFormat)) {
-		action = 'video';
-		compatibleFormats = formatsVideo;
+  // SETTINGS
 
-		if (userQuality === 'high') {
-			quality = ['-b:v', '2M', '-b:a', '128k'];
-		} else if (userQuality === 'mid') {
-			quality = ['-b:v', '1M', '-b:a', '128k'];
-		} else if (userQuality === 'low') {
-			quality = ['-b:v', '512k', '-b:a', '64k'];
-		}
-	} else if (['mp3', 'ogg'].includes(userFormat)) {
-		action = 'audio';
-		compatibleFormats = formatsAudio;
+  if (["webp", "png", "avif", "jpg", "jpeg", "jfif"].includes(userFormat)) {
+    action = "image";
+    compatibleFormats = formatsImage;
 
-		if (userQuality === 'high') {
-			quality = ['-b:a', '192k'];
-		} else if (userQuality === 'mid') {
-			quality = ['-b:a', '128k'];
-		} else if (userQuality === 'low') {
-			quality = ['-b:a', '64k'];
-		}
-	} else {
-		console.error('Unknown output format');
-		process.exit(0);
-	}
+    if (userQuality === "high") {
+      quality = 90;
+    } else if (userQuality === "mid") {
+      quality = 80;
+    } else if (userQuality === "low") {
+      quality = 60;
+    }
+  } else if (["webm", "mp4"].includes(userFormat)) {
+    action = "video";
+    compatibleFormats = formatsVideo;
 
-	if (userFormat === 'webm') {
-		extra = ['-c:v', 'libvpx', '-c:a', 'libvorbis', '-row-mt', '1', '-f', 'webm'];
-	} else if (userFormat === 'mp4') {
-		extra = ['-c:a', 'libvorbis'];
-	} else if (userFormat === 'ogg') {
-		extra = ['-vn', '-c:a', 'libvorbis'];
-	} else if (userFormat === 'mp3') {
-		extra = ['-vn'];
-	}
+    if (userQuality === "high") {
+      quality = ["-b:v", "2M", "-b:a", "96k"];
+    } else if (userQuality === "mid") {
+      quality = ["-b:v", "1M", "-b:a", "96k"];
+    } else if (userQuality === "low") {
+      quality = ["-b:v", "512k", "-b:a", "96k"];
+    }
+  } else if (["mp3", "ogg"].includes(userFormat)) {
+    action = "audio";
+    compatibleFormats = formatsAudio;
 
-	// FILES
+    if (userQuality === "high") {
+      quality = ["-b:a", "160k"];
+    } else if (userQuality === "mid") {
+      quality = ["-b:a", "128k"];
+    } else if (userQuality === "low") {
+      quality = ["-b:a", "96k"];
+    }
+  } else {
+    console.error("Unknown output format");
+    process.exit(0);
+  }
 
-	const inputDir = path.normalize(userFolder);
-	const outputDir = path.join(userFolder, 'output');
+  if (userFormat === "webm") {
+    extra = ["-c:v", "libvpx", "-c:a", "libopus", "-row-mt", "1", "-f", "webm"];
+  } else if (userFormat === "mp4") {
+    extra = ["-c:a", "libopus"];
+  } else if (userFormat === "ogg") {
+    extra = ["-vn", "-c:a", "libopus"];
+  } else if (userFormat === "mp3") {
+    extra = ["-vn"];
+  }
 
-	const files = await fsp.readdir(inputDir);
+  // FILES
 
-	const compatibleFiles = files.filter((file) =>
-		compatibleFormats.includes(path.extname(file).toLowerCase())
-	);
+  const inputDir = path.normalize(userFolder);
+  const outputDir = path.join(userFolder, "output");
 
-	if (compatibleFiles.length === 0) {
-		console.error('No compatible files found');
-		process.exit(0);
-	}
+  const files = await fsp.readdir(inputDir);
 
-	async function createOutputDir() {
-		try {
-			await fsp.mkdir(outputDir);
-		} catch (error) {
-			if (error.code === 'EEXIST') {
-				console.log('Output directory already exists');
-			} else {
-				console.error(error);
-				process.exit(1);
-			}
-		}
-	}
+  const compatibleFiles = files.filter((file) =>
+    compatibleFormats.includes(path.extname(file).toLowerCase())
+  );
 
-	await createOutputDir();
+  if (compatibleFiles.length === 0) {
+    console.error("No compatible files found");
+    process.exit(0);
+  }
 
-	const s = spinner();
+  async function createOutputDir() {
+    try {
+      await fsp.mkdir(outputDir);
+    } catch (error) {
+      if (error.code === "EEXIST") {
+        console.log("Output directory already exists");
+      } else {
+        console.error(error);
+        process.exit(1);
+      }
+    }
+  }
 
-	s.start(`Converting ${compatibleFiles.length} file(s) into ${userFormat}`);
+  await createOutputDir();
 
-	if (action === 'video' || action === 'audio') {
-		// FFMPEG
-		const ffmpeg = ffmpegFluent;
+  // FUNCTIONS
 
-		ffmpeg.setFfmpegPath(ffmpegPath);
+  async function transcodeFile(inputFile, outputFile, userFormat) {
+    return new Promise((resolve, reject) => {
+      const writeStream = fs.createWriteStream(outputFile);
+      ffmpeg(inputFile)
+        .addOutputOptions([
+          "-movflags +frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov",
+          ...quality,
+          ...extra,
+        ])
+        .format(userFormat)
+        .on("end", () => {
+          writeStream.end();
+          resolve();
+        })
+        .on("error", (err, stdout, stderr) => {
+          console.log("An error occurred: " + err.message);
+          console.log("ffmpeg output:\n" + stdout);
+          console.log("ffmpeg stderr:\n" + stderr);
+          writeStream.end();
+          reject(err);
+        })
+        .pipe(writeStream);
+    });
+  }
 
-		for (let i = 0; i < compatibleFiles.length; i++) {
-			const inputFile = path.join(inputDir, compatibleFiles[i]);
+  s.start(`Converting ${compatibleFiles.length} file(s) into ${userFormat}`);
 
-			const outputFile = path.join(
-				outputDir,
-				`${compatibleFiles[i].replace(/\.[^/.]+$/, '')}.${userFormat}`
-			);
+  if (action === "video" || action === "audio") {
+    const transcodePromises = [];
 
-			const writeStream = fs.createWriteStream(outputFile);
+    for (const file of compatibleFiles) {
+      const inputFile = path.join(inputDir, file);
+      const outputFile = path.join(
+        outputDir,
+        `${file.replace(/\.[^/.]+$/, "")}.${userFormat}`
+      );
 
-			async function transcode() {
-				return new Promise((resolve, reject) => {
-					ffmpeg(inputFile)
-						.addOutputOptions([
-							'-movflags +frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov',
-							...quality,
-							...extra
-						])
-						.format(userFormat)
-						.on('end', () => {
-							resolve();
-						})
-						.on('error', (err, stdout, stderr) => {
-							console.log('An error occurred: ' + err.message);
-							console.log('ffmpeg output:\n' + stdout);
-							console.log('ffmpeg stderr:\n' + stderr);
-							reject(err);
-						})
-						.pipe(writeStream);
-				});
-			}
+      transcodePromises.push(transcodeFile(inputFile, outputFile, userFormat));
+    }
 
-			await transcode();
-		}
-	}
+    try {
+      await Promise.all(transcodePromises);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	if (action === 'image') {
-		for (let i = 0; i < compatibleFiles.length; i++) {
-			const inputFile = path.join(inputDir, compatibleFiles[i]);
-			const outputFile = path.join(
-				outputDir,
-				`${compatibleFiles[i].replace(/\.[^/.]+$/, '')}.${userFormat}`
-			);
+  if (action === "image") {
+    for (const file of compatibleFiles) {
+      const inputFile = path.join(inputDir, file);
+      const outputFile = path.join(
+        outputDir,
+        `${file.replace(/\.[^/.]+$/, "")}.${userFormat}`
+      );
 
-			let sharpObject = sharp(inputFile);
+      let sharpObject = sharp(inputFile);
 
-			if (userFormat === 'jpeg') {
-				sharpObject = sharpObject.jpeg({ quality: quality });
-			} else if (userFormat === 'webp') {
-				sharpObject = sharpObject.webp({ quality: quality });
-			}
+      if (userFormat === "jpeg") {
+        sharpObject = sharpObject.jpeg({ quality: quality });
+      } else if (userFormat === "webp") {
+        sharpObject = sharpObject.webp({ quality: quality });
+      }
 
-			const metadata = await sharpObject.metadata();
+      try {
+        const metadata = await sharpObject.metadata();
+        const { width, height } = metadata;
 
-			const { width, height } = metadata;
+        if (width > 16383 || height > 16383) {
+          sharpObject = sharpObject.resize(16383);
+          console.log("Image is > 16393px and was resized for compatibility.");
+        }
 
-			if (width > 16383 || height > 16383) {
-				sharpObject = sharpObject.resize(16383);
-			}
+        await sharpObject.toFile(outputFile);
+      } catch (error) {
+        console.error("Error processing image:", error);
+      }
+    }
+  }
 
-			await sharpObject.toFile(outputFile);
-		}
-	}
-	s.stop(`Converted ${compatibleFiles.length} file(s)`);
+  s.stop(`Converted ${compatibleFiles.length} file(s)`);
 }
--- bin/index.js
+++ bin/index.js
@@ -1,60 +1,60 @@
 #! /usr/bin/env node
-import { intro, select, isCancel, cancel, text, outro } from '@clack/prompts';
-import { convert } from './convert.js';
+import { intro, select, isCancel, cancel, text, outro } from "@clack/prompts";
+import { convert } from "./convert.js";
 
 async function main() {
-	console.log();
-	intro(' [ VTTC 1.5.0 ] ');
+  console.log();
+  intro(" [ VTTC 1.7.6 ] ");
 
-	const folder = await text({
-		message: 'Select an input folder',
-		placeholder: './',
-		defaultValue: './',
-		initialValue: './'
-	});
+  const folder = await text({
+    message: "Select an input folder",
+    placeholder: "./",
+    defaultValue: "./",
+    initialValue: "./",
+  });
 
-	if (isCancel(folder)) {
-		cancel('Operation cancelled');
-		return process.exit(0);
-	}
+  if (isCancel(folder)) {
+    cancel("Operation cancelled");
+    return process.exit(0);
+  }
 
-	const format = await select({
-		message: 'Select an output format',
-		options: [
-			{ value: 'webp', label: 'WEBP', hint: 'Recommended for images' },
-			{ value: 'png', label: 'PNG' },
-			{ value: 'jpeg', label: 'JPEG' },
-			{ value: 'webm', label: 'WEBM', hint: 'Recommended for video' },
-			{ value: 'mp4', label: 'MP4' },
-			{ value: 'ogg', label: 'OGG', hint: 'Recommended for audio' },
-			{ value: 'mp3', label: 'MP3' }
-		]
-	});
+  const format = await select({
+    message: "Select an output format",
+    options: [
+      { value: "webp", label: "WEBP", hint: "Recommended for images" },
+      { value: "png", label: "PNG" },
+      { value: "jpeg", label: "JPEG" },
+      { value: "webm", label: "WEBM", hint: "Recommended for video" },
+      { value: "mp4", label: "MP4" },
+      { value: "ogg", label: "OGG", hint: "Recommended for audio" },
+      { value: "mp3", label: "MP3" },
+    ],
+  });
 
-	if (isCancel(format)) {
-		cancel('Operation cancelled');
-		return process.exit(0);
-	}
+  if (isCancel(format)) {
+    cancel("Operation cancelled");
+    return process.exit(0);
+  }
 
-	const quality = await select({
-		message: 'Select a quality',
-		options: [
-			{ value: 'high', label: 'HIGH' },
-			{ value: 'mid', label: 'MEDIUM', hint: 'Recommended' },
-			{ value: 'low', label: 'LOW' }
-		],
-		initialValue: 'mid'
-	});
+  const quality = await select({
+    message: "Select a quality",
+    options: [
+      { value: "high", label: "HIGH" },
+      { value: "mid", label: "MEDIUM", hint: "Recommended" },
+      { value: "low", label: "LOW" },
+    ],
+    initialValue: "mid",
+  });
 
-	if (isCancel(quality)) {
-		cancel('Operation cancelled');
-		return process.exit(0);
-	}
+  if (isCancel(quality)) {
+    cancel("Operation cancelled");
+    return process.exit(0);
+  }
 
-	await convert(folder, format, quality);
+  await convert(folder, format, quality);
 
-	outro("Your converted files are in the 'output' folder.");
+  outro("Your converted files are in the 'output' folder.");
 
-	process.exit(0);
+  process.exit(0);
 }
 main().catch(console.error);
